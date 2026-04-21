5624e4aeae1f4c4f0f9d3b0c447c03f5d1f839fc
FabrizioTC06 <fabrizioteodorcelis06@gmail.com>
2026-04-21 05:22:18 -0500

    fix: implementar CRUD y pruebas de Usuario aplicando herencia JOINED y estandar de nomenclaturas

--- src/main/java/com/hardpc/saas/backendapi/repository/UsuarioRepository.java
+++ src/main/java/com/hardpc/saas/backendapi/repository/UsuarioRepository.java
@@ -1,5 +1,9 @@
 package com.hardpc.saas.backendapi.repository;
 
+import com.hardpc.saas.backendapi.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 }
--- src/main/java/com/hardpc/saas/backendapi/service/UsuarioService.java
+++ src/main/java/com/hardpc/saas/backendapi/service/UsuarioService.java
@@ -1,17 +1,12 @@
 package com.hardpc.saas.backendapi.service;
 
-import java.util.List;
 import com.hardpc.saas.backendapi.entity.Usuario;
+import java.util.List;
 
 public interface UsuarioService {
-
-    List<Usuario> listar();
-
-    Usuario obtener(Long id);
-
-    Usuario guardar(Usuario usuario);
-
+    List<Usuario> listarTodos();
+    Usuario buscarPorId(Long id);
+    Usuario crear(Usuario usuario);
     Usuario actualizar(Long id, Usuario usuario);
-
     void eliminar(Long id);
 }
